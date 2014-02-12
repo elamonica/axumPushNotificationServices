61177b46bd7eb2e3526fc7045c8a4ea4bea89d81
Ezequiel Lamonica <lamonicaezequiel@gmail.com>
2014-02-12 10:32:51 -0300

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AxumPushNotificationServices::Application.config.secret_key_base = '7e75151aaada22daeb6636028c6665369796139f0633ede6f2403d79d9dc287f3e880fed231e1cb919820b7fe14caa2336912a926f76aa3219c38609f3f9fca5'
